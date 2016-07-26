46bef991b1c33c652aff7c1c325bb35d52341a87
johnzhd <johnzhd@126.com>
2016-07-26 15:00:56 +0800

    First version

--- gpsmap/build.sh
+++ gpsmap/build.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 
+# prepare
+
+mkdir /tmp/uwsgi
+mkdir /usr/share/gpsmap/
+mkdir /usr/share/gpsmap/uwsgi
+mkdir /usr/share/gpsmap/www
+mkdir /usr/share/gpsmap/www/static
+mkdir /usr/share/gpsmap/www/templates
+
+# website
+cp -f ./*.py              /usr/share/gpsmap/www/
+cp -f ./templates/*.html  /usr/share/gpsmap/www/templates/
+cp -f ./static/*.js       /usr/share/gpsmap/www/static/
+cp -f ./static/*.css      /usr/share/gpsmap/www/static/
+cp -f ./static/*.ico      /usr/share/gpsmap/www/static/
+
+
+# uwsgi
+cp -f ./system/emperor.ini /usr/share/gpsmap/uwsgi/emperor.ini
+cp -f ./system/emperor.uwsgi.service  /etc/systemd/system/
+
 
 # nginx
 mv  /etc/nginx/conf.d/default.conf   /etc/nginx/conf.d/default.conf.bak
+cp -f ./nginx.conf                      /etc/nginx/conf.d/
+
+
+# mongodb
+cp -f ./system/mongod.conf /etc/
+mongod -f /etc/mongod.conf
+
+mongo ./system/mongo.js
+
+mongod -f /etc/mongod.conf --shutdown
+cp -f ./system/mongod_auth.conf /etc/mongod.conf
+
+
+###
+
 
 
 
--- gpsmap/docker_start.sh
+++ gpsmap/docker_start.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# mongod
+mongod -f /etc/mongod.conf
 
+
+# uwsgi
+systemctl start emperor.uwsgi.service
+
+# nginx
+nginx -s stop
+nginx
 
 
